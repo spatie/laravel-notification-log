473b3419856ca384cdedcc3a462301ec98f39857
Freek Van der Herten <freek@spatie.be>
2023-02-25 12:58:46 +0530

    add type test

--- tests/Models/NotificationLogItemTest.php
+++ tests/Models/NotificationLogItemTest.php
@@ -7,7 +7,6 @@ beforeEach(function() {
     $this->user = User::factory()->create();
 
     $this->anotherUser = User::factory()->create();
-
 });
 
 it('can find the latest notification for a notifiable', function() {
@@ -16,8 +15,30 @@ it('can find the latest notification for a notifiable', function() {
     $secondLogItem = NotificationLogItem::factory()->forNotifiable($this->user)->create();
     $otherUserLogItem = NotificationLogItem::factory()->forNotifiable($this->anotherUser)->create();
 
-    $foundLogItem = NotificationLogItem::latestFor($this->user);
+    expect(NotificationLogItem::latestFor($this->user))->toBeModel($secondLogItem);
+    expect(NotificationLogItem::latestFor($this->anotherUser))->toBeModel($otherUserLogItem);
+});
 
-    expect($foundLogItem)->toBeModel($secondLogItem);
+it('can find the latest sent notification for a type', function() {
+    $firstType1 = NotificationLogItem::factory()->forNotifiable($this->user)->create([
+        'notification_type' => 'type1',
+    ]);
 
+    $secondType1 =NotificationLogItem::factory()->forNotifiable($this->user)->create([
+        'notification_type' => 'type1',
+    ]);
+
+   $firstType2 = NotificationLogItem::factory()->forNotifiable($this->user)->create([
+        'notification_type' => 'type2',
+    ]);
+
+    $secondType2 = NotificationLogItem::factory()->forNotifiable($this->user)->create([
+        'notification_type' => 'type2',
+    ]);
+
+    expect(NotificationLogItem::latestFor($this->user, notificationType: 'type1'))
+        ->toBeModel($secondType1);
+
+    expect(NotificationLogItem::latestFor($this->user, notificationType: 'type2'))
+        ->toBeModel($secondType2);
 });
